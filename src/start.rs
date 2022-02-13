f3b8d04d0bc560c5a1bdf35bd0c86d166534155c
tweoss <francischua689@gmail.com>
2022-02-13 07:46:43 -0800

    clean up

--- src/start.rs
+++ src/start.rs
@@ -1,33 +1,28 @@
 pub mod rustyvibes {
 
-    use std::path::PathBuf;
-use std::path::Path;
-use rdev::{listen, Event};
+    use rdev::{listen, Event};
     use serde_json;
     use serde_json::{Map, Value};
     use std::error::Error;
     use std::fs;
-    
+    use std::path::Path;
     pub use crate::keycode::key_code;
     pub use crate::play_sound::sound;
-    
     fn initialize_json(path: &str) -> Result<Map<String, Value>, Box<dyn Error>> {
         let config = fs::read_to_string(path)?;
         let parsed: Value = serde_json::from_str(&config)?;
         let obj: Map<String, Value> = parsed.as_object().unwrap().clone();
         Ok(obj)
     }
-    
     pub struct JSONFile {
         pub value: Option<serde_json::Map<std::string::String, serde_json::Value>>,
     }
-    
     impl JSONFile {
         pub fn initialize(&mut self, directory: String) {
             let soundpack_config = &format!("{}/config.json", directory)[..];
             self.value = Some(initialize_json(soundpack_config).unwrap());
         }
-        pub fn event_handler(self: &Self, event: Event, directory: String) {
+        pub fn event_handler(&self, event: Event, directory: String) {
             match &self.value {
                 Some(value) => {
                     callback(event, value.clone(), directory);
@@ -38,7 +33,6 @@ use rdev::{listen, Event};
             }
         }
     }
-    
     pub fn start_rustyvibes(args: String) {
         {
             #[cfg(any(target_os = "macos", target_os = "linux"))]
@@ -70,14 +64,16 @@ use rdev::{listen, Event};
             println!("Error: {:?}", error)
         }
     }
-    
     use once_cell::sync::Lazy;
     use std::collections::HashSet;
     use std::sync::Mutex;
-    
     static KEY_DEPRESSED: Lazy<Mutex<HashSet<i32>>> = Lazy::new(|| Mutex::new(HashSet::new()));
-    
-    fn callback(event: Event, json_file: serde_json::Map<std::string::String, serde_json::Value>, directory: String) {
+
+    fn callback(
+        event: Event,
+        json_file: serde_json::Map<std::string::String, serde_json::Value>,
+        directory: String,
+    ) {
         match event.event_type {
             rdev::EventType::KeyPress(key) => {
                 let key_code = key_code::code_from_key(key);
@@ -89,7 +85,7 @@ use rdev::{listen, Event};
                     let mut dest = match key_code {
                         Some(code) => json_file["defines"][&code.to_string()].to_string(),
                         None => {
-                            dbg!("Unmapped key: {:?}", key); // for debugging
+                            // dbg!("Unmapped key: {:?}", key); // for debugging
                             let default_key = 30; // keycode for 'a'
                             json_file["defines"][&default_key.to_string()].to_string()
                         }
